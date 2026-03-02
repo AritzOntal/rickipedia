5a710ac4f76ec101bc8e382c752711421ce675ca
Aritz Ontalvilla <aritzontal@gmail.com>
2026-03-02 14:52:23 +0100

    funcion extra retroalimentanción

--- src/components/FilterSelect.tsx
+++ src/components/FilterSelect.tsx
@@ -1,17 +1,17 @@
-// 1. Definimos qué 'props' necesita recibir desde Home.tsx
+
 interface FilterSelectProps {
-    statusFilter: string; // El estado actual ("Alive", "Dead", o vacío "")
+    statusFilter: string; 
     onFilterChange: (value: string) => void; // La función para cambiar ese estado
 }
 
 export const FilterSelect = ({ statusFilter, onFilterChange }: FilterSelectProps) => {
     return (
-        // 2. El select muestra el valor actual y reacciona cuando el usuario elige otra opción
+
         <select 
             value={statusFilter} 
             onChange={(e) => onFilterChange(e.target.value)} 
             className="filter-select"
-            style={{ padding: '8px', borderRadius: '4px', backgroundColor: '#1a1a1a', color: 'white' }}
+            style={{ padding: '8px', borderRadius: '7px', backgroundColor: '#1a1a1a', color: 'white', maxHeight: '40px' }}
         >
             <option value="">Todos los estados</option>
             <option value="Alive">Vivos</option>
